01c0f7ef3cac9cc862d21085eae40b72cc95b3d6
jacKlinc <jack@inne.io>
2024-01-23 13:04:32 +0000

    Added fuzzy locations calculation

--- app/src/constants.py
+++ app/src/constants.py
@@ -1,2 +1,4 @@
 INSTAGRAM_URL = "https://www.instagram.com/location_search/"
 INSTAGRAM_TIMEOUT = 5.0
+
+FUZZY_STD = 2
--- app/tests/test_utils.py
+++ app/tests/test_utils.py
@@ -1,14 +1,78 @@
 import os
 from dotenv import load_dotenv
 
-from ..src.utils import query_instagram
+from ..src.utils import InstagramVenue, query_instagram, calcualte_fuzzy_locations
 
 load_dotenv()
 
+lat, lng = 53.3, 6.2
+venues_example = [
+    {
+        "external_id": 108146619209136,
+        "external_id_source": "facebook_places",
+        "name": "Kollumerpomp",
+        "address": "0.5mi",
+        "lat": 53.30451,
+        "lng": 6.18946,
+    },
+    {
+        "external_id": 116713638339323,
+        "external_id_source": "facebook_places",
+        "name": "Kollumerland",
+        "address": "6mi",
+        "lat": 53.2667,
+        "lng": 6.06667,
+    },
+    {
+        "external_id": 112697452074590,
+        "external_id_source": "facebook_places",
+        "name": "Warfstermolen",
+        "address": "1.5mi",
+        "lat": 53.29576,
+        "lng": 6.2356,
+    },
+]
 
+venues_example_expected = [
+    (53.26767771872737, 6.057414932058087),
+    (53.26767771872737, 6.128707466029043),
+    (53.26767771872737, 6.2),
+    (53.26767771872737, 6.271292533970957),
+    (53.26767771872737, 6.342585067941913),
+    (53.283838859363684, 6.057414932058087),
+    (53.283838859363684, 6.128707466029043),
+    (53.283838859363684, 6.2),
+    (53.283838859363684, 6.271292533970957),
+    (53.283838859363684, 6.342585067941913),
+    (53.3, 6.057414932058087),
+    (53.3, 6.128707466029043),
+    (53.3, 6.271292533970957),
+    (53.3, 6.342585067941913),
+    (53.31616114063631, 6.057414932058087),
+    (53.31616114063631, 6.128707466029043),
+    (53.31616114063631, 6.2),
+    (53.31616114063631, 6.271292533970957),
+    (53.31616114063631, 6.342585067941913),
+    (53.332322281272624, 6.057414932058087),
+    (53.332322281272624, 6.128707466029043),
+    (53.332322281272624, 6.2),
+    (53.332322281272624, 6.271292533970957),
+    (53.332322281272624, 6.342585067941913),
+]
+
+
+# TODO add pytest to CI
 def test_query_instagram():
     """Tests if cookies work"""
     # NOTE why does this return values when 0,0  is in the middle of the atlantic?
     response_good = query_instagram(0, 0, os.getenv("INSTAGRAM_COOKIES"))
     response_bad = query_instagram(0, 0, "this is a bad cookie")
     assert (response_good is not None) & (response_bad is None)
+
+
+def test_calcualte_fuzzy_locations():
+    locs = [InstagramVenue(**v) for v in venues_example]
+    result = calcualte_fuzzy_locations(locs, lat, lng)
+    expected = venues_example_expected
+
+    assert result == expected
