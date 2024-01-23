7b61ecf7eadbc0b02c6f517a0aca8c3779d9a603
jacKlinc <jack@inne.io>
2024-01-23 11:43:58 +0000

    Instagram query tested

--- app/tests/test_utils.py
+++ app/tests/test_utils.py
@@ -5,8 +5,10 @@ from ..src.utils import query_instagram
 
 load_dotenv()
 
-COOKIES = os.getenv("INSTAGRAM_COOKIES")
-
 
 def test_query_instagram():
-    assert True
+    """Tests if cookies work"""
+    # NOTE why does this return values when 0,0  is in the middle of the atlantic?
+    response_good = query_instagram(0, 0, os.getenv("INSTAGRAM_COOKIES"))
+    response_bad = query_instagram(0, 0, "this is a bad cookie")
+    assert (response_good is not None) & (response_bad is None)
